0b1974c7ba39551930eb7b81f28c84cf78cae96d
mutalenic <nicomutale@gmail.com>
2024-03-14 06:59:35 +0200

    update cmd minize bt

--- src/cmd/cmd.js
+++ src/cmd/cmd.js
@@ -497,7 +497,5 @@ document.getElementById("help").addEventListener("click", function () {
 document.querySelector(".close").addEventListener("click", function () {
   document.querySelector(".wina").style.display = "none";
 });
-document.querySelector('.bt').addEventListener('click', function() {
-    document.querySelector('.cmd').style.display = 'none'; // Minimize the cmd prompt
-  });
+
   
